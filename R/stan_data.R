fc8e85c524d35cb706d2cc3de84ea1ef0dcb0836
ebuhle <eric.buhle@noaa.gov>
2019-04-20 21:02:31 -0700

    Rebuild documentation

--- R/stan_data.R
+++ R/stan_data.R
@@ -123,7 +123,7 @@ stan_data <- function(fish_data, fish_data_fwd = NULL, env_data = NULL,
   fish_data$year <- as.numeric(factor(fish_data$year))
   fish_data$fit_p_HOS <- as.logical(fish_data$fit_p_HOS)
   
-  if(any(unlist(tapply(fish_data$year, fish_data$pop, diff)) != 1)
+  if(any(unlist(tapply(fish_data$year, fish_data$pop, diff)) != 1))
      stop(paste0("Non-consecutive years not allowed in fish_data \n"))
   
   for(i in c("pop","year","A","fit_p_HOS","B_take_obs"))
