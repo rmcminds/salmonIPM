a5d5a1bc117ce71a6a97fa169a2316d16f29440b
ebuhle <eric.buhle@noaa.gov>
2021-05-27 19:43:46 -0700

    Make ?salmonIPM point to the function not the package

--- R/salmonIPM-package.R
+++ R/salmonIPM-package.R
@@ -1,16 +1,15 @@
 #' The 'salmonIPM' package.
 #'
-#' @description A DESCRIPTION OF THE PACKAGE
-#'
 #' @docType package
 #' @name salmonIPM-package
-#' @aliases salmonIPM
+#' @aliases salmonIPM-package
 #' @useDynLib salmonIPM, .registration = TRUE
 #' @import methods
 #' @import Rcpp
 #' @importFrom rstan sampling
+#' @keywords internal
 #'
 #' @references
 #' Stan Development Team (2020). RStan: the R interface to Stan. R package version 2.19.3. https://mc-stan.org
 #'
-NULL
+"_PACKAGE"
