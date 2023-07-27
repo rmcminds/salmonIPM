b8bcb37ef3fbf974308dd60611b65a508407ae41
ebuhle <eric.buhle@noaa.gov>
2023-07-27 16:35:36 -0700

    simplify @knitr install chunk

--- vignettes/getting-started.R
+++ vignettes/getting-started.R
@@ -10,10 +10,7 @@ if(.Platform$OS.type == "windows") options(device = "windows")
 #------------------------------
 
 ## @knitr install
-if(!require("devtools")) {
-  install.packages("devtools")
-  library("devtools")
-}
+if(!require("devtools")) install.packages("devtools")
 try(devtools::install_github("ebuhle/salmonIPM", auth_token = "my_PAT"))
 
 #------------------------------
