f36249cd7688edcd26fb2a1fe2ba9caa2c68179a
Eric Buhle <eric.buhle@noaa.gov>
2024-07-06 15:12:58 -0700

    Document salmonIPMfit-class by documenting constructor

--- R/salmonIPMfit-class.R
+++ R/salmonIPMfit-class.R
@@ -1,10 +1,18 @@
-#' Create a salmonIPMfit object
+#' Fitted model objects: the `salmonIPMfit` class
 #' 
-#' @param stanfit An object of class [rstan::`stanfit-class`].
-#' @param call An object of class `[methods::call-class]` representing the call to [salmonIPM()].
+#' Models fitted with [salmonIPM-package] are objects of S3 class `salmonIPMfit`. Elements 
+#' generally mirror the named arguments passed to the constructor function by [salmonIPM()]
+#' and are mostly self-explanatory, except `prior.info` is a named list whose elements are 
+#' lists returned by [priors] functions for all hyperparameters in the model, whether
+#' modifiable (user-specified or default) or hard-coded.
+#' 
+#' @param stanfit An object of class `stanfit`.
+#' @param call An object of class [call-class] containing the call to [salmonIPM()].
 #' @inheritParams salmonIPM
 #' 
 #' @return A salmonIPMfit object.
+#' 
+#' @name salmonIPMfit-class
 
 salmonIPMfit <- function(stanfit, call, model, life_cycle, pool_pops, SR_fun, RRS, 
                          par_models, center, scale, prior, age_S_obs, age_S_eff,
@@ -18,6 +26,3 @@ salmonIPMfit <- function(stanfit, call, model, life_cycle, pool_pops, SR_fun, RR
   
   structure(out, class = c("salmonIPMfit", "list"))
 }
-
-
-
