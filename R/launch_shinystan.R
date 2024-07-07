eb9f038ea66a5b4c68bdf0411871a082a6a20197
Eric Buhle <eric.buhle@noaa.gov>
2024-07-06 23:17:16 -0700

    salmonIPMfit method for launch_shinystan()

---
+++ R/launch_shinystan.R
@@ -0,0 +1,15 @@
+#' Launch the ShinyStan GUI with `salmonIPM` models
+#'
+#' The ShinyStan interface provides visual and numerical summaries of 
+#' model parameters and convergence diagnostics.
+#'
+#' @aliases launch_shinystan
+#' @method launch_shinystan salmonIPMfit
+#' @param object A [salmonIPMfit] object.
+#' @param ... Additional arguments to pass to [launch_shinystan.default].
+#' @seealso [shinystan::launch_shinystan()], [salmonIPM()]
+#' @importFrom shinystan launch_shinystan
+
+launch_shinystan.salmonIPMfit <- function(object, ...) {
+  launch_shinystan(object$stanfit, ...)
+}
