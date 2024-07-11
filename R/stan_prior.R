rmcminds/salmonIPM

commit 5da351caab3b0e021d9949643ab6a8772ba48e43

Attach distribution attribute to objects returned by `stan_prior()`
* Facilitate prior summary function by including the distribution name as an attribute to the named array returned by stan_prior() and thus stan_data(); Stan ignores this
* Required getting rid of my trick in stan_data() for selectively setting S-R priors to empty arrays based on RRS
* This in turn required getting rid of the conditionally zero-length prior data trick in the np Stan models, which is fine because these priors are conditionally evaluated in model{} based on RRS

To do:

* Probably better to move the conditional # Priors block from stan_data() into stan_prior() as I've considered before
* stan_prior() would then take stan_model, fish_data, RRS and user priors as args; default priors for each model would be hard-coded as is currently done in stan_data()
* Would make stan_prior() more useful, and wouldn't require duplicating as many calculations from stan_data() as I feared

R/stan_prior.R
@@ -6,18 +6,20 @@
 #' @param value A user-specified prior function as described in [`priors`].
 #' @param default The default prior function as described in [`priors`].
 #'
-#' @return A named vector of user-specifiable prior parameters whose elements are part of the
+#' @return A named array of user-specifiable prior parameters whose elements are part of the
 #' `data` argument passed to [rstan::sampling()] when fitting **salmonIPM** models.
+#' The `dist` attribute is a character string giving the name of the distribution.
 
 stan_prior <- function(value, default)
 {
   pd <- eval(default)
   if(is.null(value)) {
-    return(as.array(unlist(pd[-1])))
+    p <- pd
   } else {
     p <- eval(value)
-    stopifnot(length(p) == length(pd))
+    stopifnot(all(names(p) == names(pd)))
     stopifnot(value$dist == pd$dist)
-    return(as.array(unlist(p[-1])))
   }
+  
+  return(structure(as.array(unlist(p[-1])), dist = p$dist))
 }

R/print-summary.R
@@ -52,8 +52,8 @@ print.salmonIPMfit <- function(object, pars = "hyper", include = TRUE,
       " life cycle: ", object$life_cycle, 
       if(nchar(ages)) paste0(" (ages: ", ages, ")"),
       "\n SR_fun: ", object$SR_fun, " (RRS: ", RRS, ")\n",
-      if(nchar(par_models)) paste0(" parameter models: ", par_models),
-      "\n N = ", object$dims$N, " cases in fish_data\n",
+      if(nchar(par_models)) paste0(" parameter models: ", par_models, "\n"),
+      " N = ", object$dims$N, " cases in fish_data\n",
       " N_pop = ", N_pop, 
       if(N_pop > 1) paste0(" (", ifelse(object$pool_pops, "partial", "no"), " pooling)"),
       "\n N_year = ", object$dims$N_year, "\n\n"

R/stan_data.R
@@ -202,18 +202,12 @@ stan_data <- function(stan_model = c("IPM_SS_np","IPM_SSiter_np","IPM_SS_pp","IP
     
     if(grepl("_np", stan_model)) {
       pars <- match.arg(pars, c("alpha","alpha_W","alpha_H","Rmax","Rmax_W","Rmax_H","mu_p","mu_SS","tau"), several.ok = TRUE)
-      prior_alpha <- array(stan_prior(pdfs$alpha, lognormal(2,2)), 
-                           dim = ifelse("alpha" %in% RRS, 0, 2))
-      prior_alpha_W <- array(stan_prior(pdfs$alpha_W, lognormal(2,2)),
-                             dim = ifelse("alpha" %in% RRS, 2, 0))
-      prior_alpha_H <- array(stan_prior(pdfs$alpha_H, lognormal(2,2)),
-                             dim = ifelse("alpha" %in% RRS, 2, 0))
-      prior_Rmax <- array(stan_prior(pdfs$Rmax, lognormal(prior_Rmax_mean, prior_Rmax_sd)),
-                          dim = ifelse("Rmax" %in% RRS, 0, 2))
-      prior_Rmax_W <- array(stan_prior(pdfs$Rmax_W, lognormal(prior_Rmax_mean, prior_Rmax_sd)),
-                            dim = ifelse("Rmax" %in% RRS, 2, 0))
-      prior_Rmax_H <- array(stan_prior(pdfs$Rmax_H, lognormal(prior_Rmax_mean, prior_Rmax_sd)),
-                            dim = ifelse("Rmax" %in% RRS, 2, 0))
+      prior_alpha <- stan_prior(pdfs$alpha, lognormal(2,2))
+      prior_alpha_W <- stan_prior(pdfs$alpha_W, lognormal(2,2)) 
+      prior_alpha_H <- stan_prior(pdfs$alpha_H, lognormal(2,2))
+      prior_Rmax <- stan_prior(pdfs$Rmax, lognormal(prior_Rmax_mean, prior_Rmax_sd))
+      prior_Rmax_W <- stan_prior(pdfs$Rmax_W, lognormal(prior_Rmax_mean, prior_Rmax_sd))
+      prior_Rmax_H <- stan_prior(pdfs$Rmax_H, lognormal(prior_Rmax_mean, prior_Rmax_sd))
       prior_tau <- stan_prior(pdfs$tau, gnormal(1, 0.85, 30)) # squash tau < 0.1 to avoid divergences
     }
     
@@ -247,18 +241,12 @@ stan_data <- function(stan_model = c("IPM_SS_np","IPM_SSiter_np","IPM_SS_pp","IP
     
     if(grepl("_np", stan_model)) {
       pars <- match.arg(pars, c("alpha","Mmax","mu_MS","mu_p","tau_M","tau_S"), several.ok = TRUE)
-      prior_alpha <- array(stan_prior(pdfs$alpha, lognormal(5,5)), 
-                           dim = ifelse("alpha" %in% RRS, 0, 2))
-      prior_alpha_W <- array(stan_prior(pdfs$alpha_W, lognormal(5,5)),
-                             dim = ifelse("alpha" %in% RRS, 2, 0))
-      prior_alpha_H <- array(stan_prior(pdfs$alpha_H, lognormal(5,5)),
-                             dim = ifelse("alpha" %in% RRS, 2, 0))
-      prior_Mmax <- array(stan_prior(pdfs$Mmax, lognormal(prior_Mmax_mean, prior_Mmax_sd)),
-                          dim = ifelse("Mmax" %in% RRS, 0, 2))
-      prior_Mmax_W <- array(stan_prior(pdfs$Mmax_W, lognormal(prior_Mmax_mean, prior_Mmax_sd)),
-                            dim = ifelse("Mmax" %in% RRS, 2, 0))
-      prior_Mmax_H <- array(stan_prior(pdfs$Mmax_H, lognormal(prior_Mmax_mean, prior_Mmax_sd)),
-                            dim = ifelse("Mmax" %in% RRS, 2, 0))
+      prior_alpha <- stan_prior(pdfs$alpha, lognormal(5,5))
+      prior_alpha_W <- stan_prior(pdfs$alpha_W, lognormal(5,5))
+      prior_alpha_H <- stan_prior(pdfs$alpha_H, lognormal(5,5))
+      prior_Mmax <- stan_prior(pdfs$Mmax, lognormal(prior_Mmax_mean, prior_Mmax_sd))
+      prior_Mmax_W <- stan_prior(pdfs$Mmax_W, lognormal(prior_Mmax_mean, prior_Mmax_sd))
+      prior_Mmax_H <- stan_prior(pdfs$Mmax_H, lognormal(prior_Mmax_mean, prior_Mmax_sd))
       prior_tau_M <- stan_prior(pdfs$tau_M, gnormal(1, 0.85, 30)) # squash tau < 0.1 to avoid divergences
       prior_tau_S <- stan_prior(pdfs$tau_S, gnormal(1, 0.85, 30)) # squash tau < 0.1 to avoid divergences
     }

inst/stan/IPM_SMS_np.stan
@@ -16,14 +16,14 @@ data {
   vector[N] A;                         // habitat area associated with each spawner abundance obs
   int<lower=0> K_alpha;                // number of intrinsic productivity covariates
   matrix[N,K_alpha] X_alpha;           // intrinsic productivity covariates
-  real prior_alpha[!RRS[1]*2];         // prior meanlog, sdlog for intrinsic productivity
-  real prior_alpha_W[RRS[1]*2];        // prior meanlog, sdlog for W intrinsic productivity
-  real prior_alpha_H[RRS[1]*2];        // prior meanlog, sdlog for H intrinsic productivity
+  real prior_alpha[2];                 // prior meanlog, sdlog for intrinsic productivity
+  real prior_alpha_W[2];               // prior meanlog, sdlog for W intrinsic productivity
+  real prior_alpha_H[2];               // prior meanlog, sdlog for H intrinsic productivity
   int<lower=0> K_Mmax;                 // number of maximum smolt recruitment covariates
   matrix[N,K_Mmax] X_Mmax;             // maximum smolt recruitment covariates
-  real prior_Mmax[!RRS[2]*2];          // prior meanlog, sdlog for maximum smolt recruitment
-  real prior_Mmax_W[RRS[2]*2];         // prior meanlog, sdlog for W maximum smolt recruitment
-  real prior_Mmax_H[RRS[2]*2];         // prior meanlog, sdlog for H maximum smolt recruitment
+  real prior_Mmax[2];                  // prior meanlog, sdlog for maximum smolt recruitment
+  real prior_Mmax_W[2];                // prior meanlog, sdlog for W maximum smolt recruitment
+  real prior_Mmax_H[2];                // prior meanlog, sdlog for H maximum smolt recruitment
   int<lower=0> K_M;                    // number of smolt recruitment covariates
   row_vector[K_M] X_M[N];              // smolt recruitment covariates
   // smolt abundance

inst/stan/IPM_SMaS_np.stan
@@ -18,14 +18,14 @@ data {
   vector[N] A;                          // habitat area associated with each spawner abundance obs
   int<lower=0> K_alpha;                 // number of intrinsic productivity covariates
   matrix[N,K_alpha] X_alpha;            // intrinsic productivity covariates
-  real prior_alpha[!RRS[1]*2];          // prior meanlog, sdlog for intrinsic productivity
-  real prior_alpha_W[RRS[1]*2];         // prior meanlog, sdlog for W intrinsic productivity
-  real prior_alpha_H[RRS[1]*2];         // prior meanlog, sdlog for H intrinsic productivity
+  real prior_alpha[2];                  // prior meanlog, sdlog for intrinsic productivity
+  real prior_alpha_W[2];                // prior meanlog, sdlog for W intrinsic productivity
+  real prior_alpha_H[2];                // prior meanlog, sdlog for H intrinsic productivity
   int<lower=0> K_Mmax;                  // number of maximum smolt recruitment covariates
   matrix[N,K_Mmax] X_Mmax;              // maximum smolt recruitment covariates
-  real prior_Mmax[!RRS[2]*2];           // prior meanlog, sdlog for maximum smolt recruitment
-  real prior_Mmax_W[RRS[2]*2];          // prior meanlog, sdlog for W maximum smolt recruitment
-  real prior_Mmax_H[RRS[2]*2];          // prior meanlog, sdlog for H maximum smolt recruitment
+  real prior_Mmax[2];                   // prior meanlog, sdlog for maximum smolt recruitment
+  real prior_Mmax_W[2];                 // prior meanlog, sdlog for W maximum smolt recruitment
+  real prior_Mmax_H[2];                 // prior meanlog, sdlog for H maximum smolt recruitment
   int<lower=0> K_M;                     // number of smolt recruitment covariates
   row_vector[K_M] X_M[N];               // smolt recruitment covariates
   // smolt abundance

inst/stan/IPM_SS_np.stan
@@ -14,14 +14,14 @@ data {
   vector<lower=0>[N] A;                // habitat area associated with each spawner abundance obs
   int<lower=0> K_alpha;                // number of intrinsic productivity covariates
   matrix[N,K_alpha] X_alpha;           // intrinsic productivity covariates
-  real prior_alpha[!RRS[1]*2];         // prior meanlog, sdlog for intrinsic productivity
-  real prior_alpha_W[RRS[1]*2];        // prior meanlog, sdlog for W intrinsic productivity
-  real prior_alpha_H[RRS[1]*2];        // prior meanlog, sdlog for H intrinsic productivity
+  real prior_alpha[2];                 // prior meanlog, sdlog for intrinsic productivity
+  real prior_alpha_W[2];               // prior meanlog, sdlog for W intrinsic productivity
+  real prior_alpha_H[2];               // prior meanlog, sdlog for H intrinsic productivity
   int<lower=0> K_Rmax;                 // number of maximum recruitment covariates
   matrix[N,K_Rmax] X_Rmax;             // maximum recruitment covariates
-  real prior_Rmax[!RRS[2]*2];          // prior meanlog, sdlog for maximum recruitment
-  real prior_Rmax_W[RRS[2]*2];         // prior meanlog, sdlog for W maximum recruitment
-  real prior_Rmax_H[RRS[2]*2];         // prior meanlog, sdlog for H maximum recruitment
+  real prior_Rmax[2];                  // prior meanlog, sdlog for maximum recruitment
+  real prior_Rmax_W[2];                // prior meanlog, sdlog for W maximum recruitment
+  real prior_Rmax_H[2];                // prior meanlog, sdlog for H maximum recruitment
   int<lower=0> K_R;                    // number of recruitment covariates
   row_vector[K_R] X_R[N];              // brood-year productivity covariates
   // kelt survival

commit 6f21aa13fb55872348163151299dafdcc439e5c2

Fix bug in `stan_prior()` introduced in 5da351caab3b0e021d9949643ab6a8772ba48e43
* I switched the order of the arguments from (value, default) to (default, user) but didn't change the calls in stan_data()
* Now changed the order back to (user, default)

R/stan_prior.R
@@ -4,22 +4,22 @@
 #' prior parameters for the given `stan_model`. This function is for internal use. 
 #' 
 #' @param default The default prior function as described in [`priors`].
-#' @param value A user-specified prior function as described in [`priors`].
+#' @param user A user-specified prior function as described in [`priors`].
 #'
 #' @return A named array of user-specifiable prior parameters whose elements are part of the
 #' `data` argument passed to [rstan::sampling()] when fitting **salmonIPM** models.
 #' The `dist` attribute is a character string giving the name of the distribution.
 #' 
 #' @importFrom utils as.relistable
-stan_prior <- function(default, value = NULL)
+stan_prior <- function(user, default)
 {
   pd <- eval(default)
-  if(is.null(value)) {
+  if(is.null(user)) {
     p <- pd
   } else {
-    p <- eval(value)
+    p <- eval(user)
     stopifnot(all(names(p) == names(pd)))
-    stopifnot(value$dist == pd$dist)
+    stopifnot(user$dist == pd$dist)
   }
   
   pdist <- p$dist